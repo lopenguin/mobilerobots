96cccb86ca6806e19fec203bbe235de279184812
Lorenzo <lorenzo@caltech.edu>
2022-06-15 23:33:46 -0700

    added comment

--- src/atlas/scripts/recognition/recognize.py
+++ src/atlas/scripts/recognition/recognize.py
@@ -2,3 +2,5 @@
 # Recognizes macro-scale patterns in pointcloud data.
 # Confined to rectangles (for now)
 
+# Instead of using lines, try storing points and connections (a graph, effectively)
+# Connection includes: point, confidence (<50% means more likely no connection, >50% means more likely closed)
